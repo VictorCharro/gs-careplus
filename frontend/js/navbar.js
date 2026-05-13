b54377931bc2972ab6df1b17176c3b34d26df501
VcsDevs <v_chs@hotmail.com>
2026-05-12 22:54:11 -0300

    Atualizando navbar

--- frontend/js/navbar.js
+++ frontend/js/navbar.js
@@ -4,6 +4,7 @@ function initNavbar() {
   const btnCancelar = document.getElementById('btn-cancelar');
   const btnHome = document.getElementById('btn-home');
   const btnRecompensas = document.getElementById('btn-recompensas');
+  const btnLembretes = document.getElementById('btn-lembretes');
 
   let zoomLevel = 1;
 
@@ -37,5 +38,21 @@ function initNavbar() {
     btnRecompensas.addEventListener('click', () => {
       window.location.href = 'recompensas.html';
     });
+  }
+
+  if (btnLembretes) {
+    btnLembretes.addEventListener('click', () => {
+      window.location.href = 'lembretes.html';
+    });
+  }
+
+  const currentPage = window.location.pathname.split('/').pop();
+  
+  if (currentPage === 'cancelamento.html' && btnCancelar) {
+    btnCancelar.classList.add('navbar-action-bold');
+  } else if (currentPage === 'recompensas.html' && btnRecompensas) {
+    btnRecompensas.classList.add('navbar-action-bold');
+  } else if (currentPage === 'lembretes.html' && btnLembretes) {
+    btnLembretes.classList.add('navbar-action-bold');
   }
 }
